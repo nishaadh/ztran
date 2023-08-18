9472b528bdb9a531bd6f4aad1c92fd981c6095d5
nishaadh <134630577+nishaadh@users.noreply.github.com>
2023-08-18 16:03:04 -0500

    2UE one malisious nexran

--- zmqtwoue.sh
+++ zmqtwoue.sh
@@ -1,9 +1,4 @@
-#!/bin/sh
-
-SLEEPINT=$1
-if [ -z "$SLEEPINT" ]; then
-    SLEEPINT=4
-fi
+SLEEPINT=5;
 
 export NEXRAN_XAPP=`kubectl get svc -n ricxapp --field-selector metadata.name=service-ricxapp-nexran-rmr -o jsonpath='{.items[0].spec.clusterIP}'`
 if [ -z "$NEXRAN_XAPP" ]; then
@@ -26,16 +21,9 @@ curl -i -X GET http://${NEXRAN_XAPP}:8000/v1/ues ; echo ; echo
 sleep $SLEEPINT
 
 echo "Creating NodeB (id=1):" ; echo
-OUTPUT=`curl -X POST -H "Content-type: application/json" -d '{"type":"eNB","id":411,"mcc":"001","mnc":"01"}' http://${NEXRAN_XAPP}:8000/v1/nodebs`
-echo $OUTPUT
-NBNAME=`echo $OUTPUT | jq -r '.name'`
+curl -i -X POST -H "Content-type: application/json" -d '{"type":"eNB","id":411,"mcc":"001","mnc":"01"}' http://${NEXRAN_XAPP}:8000/v1/nodebs ; echo ; echo
 echo Listing NodeBs: ; echo
 curl -i -X GET http://${NEXRAN_XAPP}:8000/v1/nodebs ; echo ; echo
-
-if [ -z "$OUTPUT" -o -z "$NBNAME" ]; then
-    echo "ERROR: failed to create NodeB; aborting"
-    exit 1
-fi
 
 sleep $SLEEPINT
 
@@ -54,16 +42,18 @@ curl -i -X GET http://${NEXRAN_XAPP}:8000/v1/slices ; echo ; echo
 sleep $SLEEPINT
 
 echo "Binding Slice to NodeB (name=fast):" ; echo
-curl -i -X POST http://${NEXRAN_XAPP}:8000/v1/nodebs/${NBNAME}/slices/fast ; echo ; echo
-echo "Getting NodeB (name=${NBNAME}):" ; echo
-curl -i -X GET http://${NEXRAN_XAPP}:8000/v1/nodebs/${NBNAME} ; echo ; echo
+
+curl -i -X POST http://${NEXRAN_XAPP}:8000/v1/nodebs/enB_macro_001_001_0019b0/slices/fast ; echo ; echo
+echo "Getting NodeB (name=enB_macro_001_001_0019b0):" ; echo
+curl -i -X GET http://${NEXRAN_XAPP}:8000/v1/nodebs/enB_macro_001_001_0019b0 ; echo ; echo
 
 sleep $SLEEPINT
 
-echo "Binding Slice to NodeB (name=slow):" ; echo
-curl -i -X POST http://${NEXRAN_XAPP}:8000/v1/nodebs/${NBNAME}/slices/secure_slice ; echo ; echo
-echo "Getting NodeB (name=${NBNAME}):" ; echo
-curl -i -X GET http://${NEXRAN_XAPP}:8000/v1/nodebs/${NBNAME} ; echo ; echo
+echo "Binding Slice to NodeB (name=secure_slice):" ; echo
+
+curl -i -X POST http://${NEXRAN_XAPP}:8000/v1/nodebs/enB_macro_001_001_0019b0/slices/secure_slice ; echo ; echo
+echo "Getting NodeB (name=enB_macro_001_001_0019b0):" ; echo
+curl -i -X GET http://${NEXRAN_XAPP}:8000/v1/nodebs/enB_macro_001_001_0019b0 ; echo ; echo
 
 sleep $SLEEPINT
 
@@ -74,28 +64,20 @@ curl -i -X GET http://${NEXRAN_XAPP}:8000/v1/ues ; echo ; echo
 
 sleep $SLEEPINT
 
-echo "Binding Ue to Slice fast (imsi=001010123456789):" ; echo
-curl -i -X POST http://${NEXRAN_XAPP}:8000/v1/slices/fast/ues/001010123456789 ; echo ; echo
-echo "Getting Slice (name=fast):" ; echo
-curl -i -X GET http://${NEXRAN_XAPP}:8000/v1/slices/fast ; echo ; echo
-
 echo "Creating Ue (ue=001010123456780)" ; echo
 curl -i -X POST -H "Content-type: application/json" -d '{"imsi":"001010123456780"}' http://${NEXRAN_XAPP}:8000/v1/ues ; echo ; echo
 echo Listing Ues: ; echo
 curl -i -X GET http://${NEXRAN_XAPP}:8000/v1/ues ; echo ; echo
 
-echo "Binding Ue (imsi=001010123456780):" ; echo
-curl -i -X POST http://${NEXRAN_XAPP}:8000/v1/slices/fast/ues/001010123456780 ; echo ; echo
-echo "Getting Slice (name=slow):" ; echo
+sleep $SLEEPINT
+
+echo "Binding Ue to Slice fast (imsi=001010123456789):" ; echo
+curl -i -X POST http://${NEXRAN_XAPP}:8000/v1/slices/fast/ues/001010123456789 ; echo ; echo
+echo "Getting Slice (name=fast):" ; echo
 curl -i -X GET http://${NEXRAN_XAPP}:8000/v1/slices/fast ; echo ; echo
 
-#sleep $SLEEPINT
-
-#echo "Inverting priority of fast and slow slices:" ; echo
-
-#curl -i -X PUT -H "Content-type: application/json" -d '{"allocation_policy":{"type":"proportional","share":1024}}' http://${NEXRAN_XAPP}:8000/v1/slices/slow ; echo ; echo ;
-
-#sleep $SLEEPINT
-
-#curl -i -X PUT -H "Content-type: application/json" -d '{"allocation_policy":{"type":"proportional","share":256}}' http://${NEXRAN_XAPP}:8000/v1/slices/fast ; echo ; echo
+echo "Binding Ue (imsi=001010123456780):" ; echo
+curl -i -X POST http://${NEXRAN_XAPP}:8000/v1/slices/fast/ues/001010123456780 ; echo ; echo
+echo "Getting Slice (name=fast):" ; echo
+curl -i -X GET http://${NEXRAN_XAPP}:8000/v1/slices/fast; echo ; echo
 
